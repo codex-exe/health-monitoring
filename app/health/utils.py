1fb98a07a842f2e14906dbf5490d0cae7a708028
Alabhya Sharma <71889507+codex-exe@users.noreply.github.com>
2023-10-26 16:24:00 +0000

    save

--- app/health/utils.py
+++ app/health/utils.py
@@ -1,4 +1,11 @@
+import os
+
 def save_kb(kb, filename):
+    # Create the directory if it doesn't exist
+    directory = os.path.dirname(filename)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     # Save the knowledge base to a file
     with open(filename, 'w') as f:
         f.write(kb)
